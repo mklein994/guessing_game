e26cccdb3876464b06fd359f2386861db76473a6
Matthew Klein <mklein994@gmail.com>
2017-06-13 20:31:57 -0500

    Use a loop and check for correct answer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -12,10 +13,24 @@ fn main() {
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed : {}", guess);
+        println!("You guessed : {}", guess);
+
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
